cd2ca4bcbf01df3e83f22c08b5e0d44e736ebbb6
lucagez <lucagesmundo@yahoo.it>
2019-02-06 17:17:07 +0100

    fixed test accordingly to multi selection

--- test/subobj.test.js
+++ test/subobj.test.js
@@ -5,53 +5,57 @@ const subobj = require('../dist/subobj');
 
 describe('subobj', () => {
   const { expect } = chai;
+  const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
 
-  it('should return a string', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
+  it('should return an array', () => {
     const sub = subobj(data, 'Pigiami');
-    assert.equal(typeof sub, 'string');
+    expect(sub).to.be.an('array');
   });
+
   it('should return a valid JSON', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
-    const sub = subobj(data, 'Pigiami');
+    const sub = subobj(data, 'Pigiami')[0];
     const obj = JSON.parse(sub);
     assert.equal(typeof obj, 'object');
   });
+
   it('should parse correctly properties equal to numbers', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
-    const sub = subobj(data, 'Pigiami');
+    const sub = subobj(data, 'Pigiami')[0];
     const obj = JSON.parse(sub);
     const num = parseInt(obj.Pigiami, 10);
     assert.equal(typeof num, 'number');
   });
+
   it('should parse correctly properties equal to strings', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
-    const sub = subobj(data, 'Kimono');
+    const sub = subobj(data, 'Kimono')[0];
     const str = JSON.parse(sub).Kimono;
     assert.equal(typeof str, 'string');
   });
+
   it('should parse correctly properties equal to objects', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
-    const sub = subobj(data, 'Abbigliamento e accessori');
+    const sub = subobj(data, 'Abbigliamento e accessori')[0];
     const obj = JSON.parse(sub);
     assert.equal(typeof obj, 'object');
   });
+
   it('should parse correctly properties equal to arrays', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
     const arrays = {
-      first: JSON.parse(subobj(data, 'Gonne')).Gonne,
-      second: JSON.parse(subobj(data, 'Obi')).Obi
+      first: JSON.parse(subobj(data, 'Gonne')[0]).Gonne,
+      second: JSON.parse(subobj(data, 'Obi')[0]).Obi
     };
     expect(arrays.first).to.be.an('array');
     expect(arrays.second).to.be.an('array');
   });
-  it('should thow an error when a non-existent property is searched', () => {
-    const data = fs.readFileSync(`${__dirname}/data/google_taxonomy.json`, 'UTF-8');
-    expect(() => subobj(data, 'Kimondo')).to.throw();
+
+  it('should return an empty array when a non-existent property is searched', () => {
+    const sub = subobj(data, 'Kimondo');
+    expect(sub)
+      .to.be.an('array')
+      .to.have.length(0);
   });
+
   it('works in pathMode', () => {
     const path = `${__dirname}/data/google_taxonomy.json`;
-    const sub = subobj(path, 'Pigiami', { pathMode: true });
+    const sub = subobj(path, 'Articoli', { pathMode: true })[0];
     const obj = JSON.parse(sub);
     assert.equal(typeof obj, 'object');
   });
