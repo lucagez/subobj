64c4889aa4e3986fe291bfc90e5194c8b949e0fa
lucagez <lucagesmundo@yahoo.it>
2019-02-11 18:51:45 +0100

    found effective stream parsing method

--- test/stream.test.js
+++ test/stream.test.js
@@ -1,86 +1,104 @@
+// the stream method is a lot slow than in memory method.
+
 const fs = require('fs');
-const Stream = require('stream');
+const subobj = require('../dist/subobj');
 
 const path = `${__dirname}/data/google_taxonomy.json`;
 
-const testStream = new Stream.Writable();
-const charStream = new Stream.Writable();
-
-charStream._write = (char, encoding, next) => {
-  // console.log(char.toString());
-  // char.toString();
-  next();
+const store = {
+  overlap: '',
+  overlapOBJ: ''
 };
 
-testStream._write = (chunk, encoding, next) => {
-  const string = chunk.toString();
-  // // console.log(string);
-  // for (const char of string) {
-  //   charStream.write(char);
-  // }
-  let i = 0;
-  const len = chunk.length;
-  for (; i < len; ++i) {
-    charStream.write(string.charAt(i));
+// test term for overlapping test (object on two different chunks)
+// const term = 'Cambio pannolini';
+const term = 'Articoli';
+
+// init
+const find = (string, search) => {
+  const regex = RegExp(`"${search}":`, 'g');
+  const accumulator = [];
+  let temp;
+  while ((temp = regex.exec(string)) !== null) {
+    accumulator.push(temp.index);
+  }
+  return accumulator;
+};
+
+// preferring for-loop over recursion to avoid `max call stack size exceeded error`
+const matchBracket = (string, len, brackets) => {
+  let counter = 0;
+  let index = 0;
+  let flag = false;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const char of string) {
+    if (char === brackets[0]) {
+      counter += 1;
+      flag = true;
+    } else if (char === brackets[1]) counter -= 1;
+    if (counter === 0) if (flag) break;
+    index += 1;
   }
 
-  next();
+  return {
+    end: counter === 0,
+    index
+  };
 };
 
+const parse = (string, len, index = 0) => {
+  if (index === len) return { end: false, string };
+  const current = string.charAt(index);
+  if (current === ',' || current === '}') return { end: true, string: string.substr(0, index) };
+  if (current === '[' || current === '{') {
+    const brackets = current === '[' ? ['[', ']'] : ['{', '}'];
+    const matchedIndex = matchBracket(string, len, brackets);
+    const sub = string.substr(0, matchedIndex.index);
+    return {
+      end: matchedIndex.end,
+      // postponing a bracket if the match is complete
+      string: sub + (matchedIndex.end ? brackets[1] : '')
+    };
+  }
+  return parse(string, len, (index += 1));
+};
+
+// test time
 const t = Date.now();
-fs.createReadStream(path).pipe(testStream);
+const stream = fs.createReadStream(path);
 
-testStream.on('finish', () => console.log(Date.now() - t));
+stream.on('data', data => {
+  console.log('\n ######################## \n');
 
+  // returning a string that is equal to the actual chunk plus the optional
+  // overlap found in previous `data` iteration
+  const string = (store.overlapOBJ !== '' ? store.overlapOBJ : store.overlap) + data.toString();
+
+  // overlapping
+  const len = string.length;
+  const cut = len - `"${term}":`.length - 1;
+  store.overlap = string.substr(cut);
+  console.log(string.substr(0, 20));
+
+  const indexes = find(string, term);
+  indexes.forEach(i => {
+    const substr = string.substr(i);
+    const extract = parse(substr, len);
+
+    // updating global store to contain overlapping objects across chunks
+    store.overlapOBJ = !extract.end ? extract.string : '';
+
+    // modify flow to return only completed strings
+    console.log(parse(substr, len));
+  });
+});
+
+stream.on('end', () => {
+  console.log('stream:', Date.now() - t);
+});
+
+// in memory test
 const t1 = Date.now();
-JSON.parse(fs.readFileSync(path));
-console.log(Date.now() - t1)
-// class Find {
-//   constructor(term) {
-//     this.term = term.split('');
-//     this.target = this.term.length;
-//     this.index = 0;
-//     this.current = this.term[0];
-//   }
-
-//   search(char) {
-//     if (char === this.current) {
-//       this.index += 1;
-//       this.current = this.term[this.index];
-//       console.log(this.current);
-//       if (this.index === this.target) return;
-//     }
-//     this.index = 0;
-//   }
-// }
-
-// const store = {
-//   term = '',
-//   target = '',
-//   index = 0,
-//   current = term[0]
-// };
-
-// const createStore = string => {
-//   const term = string.split('');
-//   const target = term.length;
-//   const index = 0;
-//   const current = term[index];
-//   return {
-//     term,
-//     target,
-//     index,
-//     current
-//   }
-// }
-
-// // const updateStore = (obj, )
-
-// let count = 0;
-// stream.on('data', data => {
-//   count += 1;
-//   // console.log(data);
-
-// });
-
-// stream.on('end', () => console.log(count));
+const data = fs.readFileSync(path, 'UTF-8');
+subobj(data, term);
+console.log('in memory:', Date.now() - t1);
