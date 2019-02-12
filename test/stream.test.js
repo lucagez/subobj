86bfbadd4fc84d330446a9f132eaf49142596913
lucagez <lucagesmundo@yahoo.it>
2019-02-12 11:17:48 +0100

    working stream with HUGE json files

--- test/stream.test.js
+++ test/stream.test.js
@@ -7,7 +7,10 @@ const subobj = require('../dist/subobj');
 // const path = `${__dirname}/data/google_taxonomy.json`;
 
 // test with BIG file
-const path = `${__dirname}/data/citylots.json`;
+// const path = `${__dirname}/data/citylots.json`;
+
+// test with HUGE file
+const path = `${__dirname}/data/huge.json`;
 
 const store = {
   overlap: '',
@@ -16,7 +19,9 @@ const store = {
 
 // test term for overlapping test (object on two different chunks)
 // const term = 'Cambio pannolini';
-const term = 'BLKLOT';
+const term = 'Articoli';
+// const term = 'GEZ';
+// const term = 'type';
 let chunkCounter = 0;
 
 // init
@@ -68,44 +73,79 @@ const parse = (string, len, index = 0) => {
   return parse(string, len, (index += 1));
 };
 
-// test time
-const t = Date.now();
-const stream = fs.createReadStream(path, { highWaterMark: 8192 * 1024 });
+const forParse = (string, len, index = 0) => {
+  let parsed = {};
+  for (; index < len;) {
+    const current = string.charAt(index);
+    // if (index === len) {
+    //   parsed = { end: false, string };
+    //   break;
+    // };
+    if (current === ',' || current === '}') {
+      parsed = { end: true, string: string.substr(0, index) };
+      break;
+    };
+    if (current === '[' || current === '{') {
+      const brackets = current === '[' ? ['[', ']'] : ['{', '}'];
+      const matchedIndex = matchBracket(string, len, brackets);
+      const sub = string.substr(0, matchedIndex.index);
+      // postponing a bracket if the match is complete
+      parsed = {
+        end: matchedIndex.end,
+        string: sub + (matchedIndex.end ? brackets[1] : '')
+      };
+      break;
+    }
+    index += 1;
+  }
+  if (!parsed.end) parsed = { end: false, string };
+  return parsed;
+};
 
-stream.on('data', data => {
-  chunkCounter += 1;
-  // console.log('\n ######################## \n');
+// // test time
+// const t = Date.now();
+// const stream = fs.createReadStream(path, { highWaterMark: 8192 * 1024 });
 
-  // returning a string that is equal to the actual chunk plus the optional
-  // overlap found in previous `data` iteration
-  const string = (store.overlapOBJ !== '' ? store.overlapOBJ : store.overlap) + data.toString();
+// stream.on('data', data => {
+//   chunkCounter += 1;
+//   // console.log('\n ######################## \n');
 
-  // overlapping
-  const len = string.length;
-  const cut = len - `"${term}":`.length - 1;
-  store.overlap = string.substr(cut);
-  // console.log(string.substr(0, 20));
+//   // returning a string that is equal to the actual chunk plus the optional
+//   // overlap found in previous `data` iteration
+//   const string = (store.overlapOBJ !== '' ? store.overlapOBJ : store.overlap) + data.toString();
 
-  const indexes = find(string, term);
-  indexes.forEach(i => {
-    const substr = string.substr(i);
-    const extract = parse(substr, len);
+//   // overlapping
+//   const len = string.length;
+//   const cut = len - `"${term}":`.length - 1;
+//   store.overlap = string.substr(cut);
+//   // console.log(string.substr(0, 20));
 
-    // updating global store to contain overlapping objects across chunks
-    store.overlapOBJ = !extract.end ? extract.string : '';
+//   const indexes = find(string, term);
+//   indexes.forEach(i => {
+//     const substr = string.substr(i);
+//     const extract = forParse(substr, len);
 
-    // modify flow to return only completed strings
-    // console.log(parse(substr, len));
-  });
-});
+//     // updating global store to contain overlapping objects across chunks
+//     store.overlapOBJ = !extract.end ? extract.string : '';
+//     // if (extract.end) console.log(extract.string);
+//     // modify flow to return only completed strings
+//     // console.log(parse(substr, len));
+//   });
+// });
 
-stream.on('end', () => {
-  console.log('stream:', Date.now() - t);
-  console.log('Data divided in', chunkCounter);
-});
+// stream.on('end', () => {
+//   console.log('stream:', Date.now() - t);
+//   console.log('Data divided in', chunkCounter);
+//   const mem = process.memoryUsage().heapUsed / 1024 / 1024;
+//   console.log(`The script uses approximately ${mem.toFixed(1)} MB`);
+// });
 
 // in memory test
 // const t1 = Date.now();
 // const data = fs.readFileSync(path, 'UTF-8');
 // subobj(data, term);
 // console.log('in memory:', Date.now() - t1);
+
+// // memory usage
+// const mem = process.memoryUsage().heapUsed / 1024 / 1024;
+// console.log(`The script uses approximately ${mem.toFixed(1)} MB`);
