88a480816fadf0300d37a352afc0eb8ab5982053
lucagez <lucagesmundo@yahoo.it>
2019-02-13 15:08:29 +0100

    find occurrences

--- test/buffer.test.js
+++ test/buffer.test.js
@@ -6,8 +6,8 @@ const read = util.promisify(fs.read);
 const close = util.promisify(fs.close);
 
 // const path = `${__dirname}/data/google_taxonomy.json`;
-// const path = `${__dirname}/data/citylots.json`;
-const path = `${__dirname}/data/huge.json`;
+const path = `${__dirname}/data/citylots.json`;
+// const path = `${__dirname}/data/huge.json`;
 
 const forro = data => {
   return new Promise((resolve, reject) => {
@@ -19,12 +19,33 @@ const forro = data => {
   });
 }
 
+const find = (buffer, search) => {
+  const searchArr = search.split('').map(e => e.charCodeAt(0));
+  const target = searchArr.length;
+  let index = 0;
+  let acc = '';
+  for (const char of buffer) {
+    const current = searchArr[index];
+    if (char === current) {
+      index += 1;
+      acc += String.fromCharCode(char);
+      if (index === target) break;
+    }
+  }
+  return acc;
+}
+
 (async () => {
+  const search = 'BLKLOT';
+  const term = `"${search}":`;
+
   const t = Date.now();
   const bsize = 4096 * 1024;
   const buffer = Buffer.alloc(bsize);
   const f = await open(path, 'r');
   let i = 0;
+
+  let counter = 0;
   for(;;) {
     const data = await read(f, buffer, 0, bsize, i * bsize);
     
@@ -34,7 +55,11 @@ const forro = data => {
 
     // const search = 0;
     // const index = data.buffer.indexOf(search);
-    // console.log(data.buffer.indexOf(0340650980981051031080));
+    if (data.buffer.indexOf(term) !== -1) {
+      find(data.buffer, term);
+      // console.log(find(data.buffer, term));
+      counter += 1;
+    }
 
     // console.log(data)
 
@@ -42,6 +67,7 @@ const forro = data => {
     i += 1;
   }
   await close(f);
+  console.log(counter);
   // const mem = process.memoryUsage().heapUsed / 1024 / 1024;
   // console.log(`The script uses approximately ${mem.toFixed(1)} MB`);
   console.log(Date.now() - t);
